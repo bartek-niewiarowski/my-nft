81830e6d0c9501f88960b69b9fe0dbe465afed30
t-slowinski <88034336+t-slowinski@users.noreply.github.com>
2023-05-31 14:52:05 +0200

    Added contract address constraint to ownership checking

--- scripts/check.js
+++ scripts/check.js
@@ -10,7 +10,10 @@ const alchemy = new Alchemy.Alchemy ({
 
 exports.verifyOwnership = async (tokenId, address) => {
     console.log(address);
-    const tokens = await alchemy.nft.getNftsForOwner(address);
+    const tokens = await alchemy.nft.getNftsForOwner(
+        address,
+        {contractAddresses: [process.env.CONTRACT_ADDRESS]}
+    );
     const parsedToken = parseHash(tokenId);
     for(let token of tokens['ownedNfts']) {
         if(bignumber(token.tokenId, 10).isEqualTo(parsedToken))
